b4fcc277121f523b986cf2e05e254b2c4ee052de
Try2doo <67353457+Try2doo@users.noreply.github.com>
2023-07-17 18:28:00 -0500

    Update Q0306.py
    added t3  and t4

--- Q0306.py
+++ Q0306.py
@@ -7,6 +7,18 @@ print(t3)                      #(1, 6, 9, 4, 3, 2, 7, 8, 3, 5)
 print(f"t3 must have :", t3)   #t3 must have : (1, 6, 9, 4, 3, 2, 7, 8, 3, 5)
 
 
+t1 = (1, 6, 9, 4, 3,)
+t2 = (2, 7, 8, 3, 5)
+t3 = t1 + t2
+t4 = t3+t2
+print(t3)                      #(1, 6, 9, 4, 3, 2, 7, 8, 3, 5)
+print(f"t3 must have :", t3)   #t3 must have : (1, 6, 9, 4, 3, 2, 7, 8, 3, 5)
+print(f" t4 must have :", t4)
+
+
+
+
+
 
 
 
